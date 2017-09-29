72e6560550066d619ed02e1a96dd2bd399582cd5
Alex Dovenmuehle <adovenmuehle@gmail.com>
2017-09-29 00:28:51 -0400

    Added ability to vote for 4 options, changed to typescript.

--- start.ts
+++ start.ts
@@ -1,5 +1,6 @@
 var tmi = require("tmi.js");
 var storage = require('node-persist');
+
 var options = {
     options: {
         debug: true
@@ -13,82 +14,87 @@ var options = {
     },
     channels: ["#programmingpeople"]
 };
+
 var client = new tmi.client(options);
+
 // Connect the client to the server..
 client.connect();
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
 var runAndTellThat = function (message) {
     client.say("#programmingpeople", message);
-};
-var getYourFate = function () {
+}
+
+var getYourFate = function() {
     return getRandomInt(1, 20);
-};
-function voteAnnouncement(votes) {
+}
+
+function voteAnnouncement(votes: Object) {
     var s = [];
     for (var prop in votes) {
-        s.push(prop + " has " + votes[prop] + " votes ");
+        s.push(`${prop} has ${votes[prop]} votes `);
     }
     return s.join(',');
 }
+
 var choices = ["big timer bot", "Paintball Buy/Sell/Trade", "Bitcoin miner", "chat game"];
-var votes = new Map();
+var votes: Map<string, number> = new Map<string, number>()
 for (var choice in choices) {
     votes.set(choice, 0);
 }
-var voted = [];
+var voted: Array<string> = [];
+
 storage.init().then(function () {
     console.log('storage is init');
-    var storedVotes = storage.getItemSync('votes');
-    var storedVoted = storage.getItemSync('voted');
-    if (storedVotes)
-        votes = storedVotes;
-    if (storedVoted)
-        voted = storedVoted;
+    const storedVotes = storage.getItemSync('votes');
+    const storedVoted: string[] = storage.getItemSync('voted');
+    if (storedVotes) votes = storedVotes;
+    if (storedVoted) voted = storedVoted;
+    
     client.on("join", function (channel, username, self) {
-        if (self)
-            return; // don't announce ourselves
-        if (username == "programmingpeople")
-            return; // don't announce the automod
-        client.say("#programmingpeople", "what is up " + username + "? You need to !roll playa");
+        if (self) return; // don't announce ourselves
+        if (username == "programmingpeople") return; // don't announce the automod
+
+        client.say("#programmingpeople", `what is up ${username}? You need to !roll playa`)
     });
+
     client.on("message", function (channel, userstate, message, self) {
-        if (self)
-            return;
+        if (self) return;
+
         if (message.startsWith("!roll")) {
-            client.say("#programmingpeople", "You rolled a " + getYourFate() + "!");
+            client.say("#programmingpeople", `You rolled a ${getYourFate()}!`);
         }
         if (message.startsWith("!vote")) {
-            var name_1 = userstate.username;
+            const name = userstate.username;
             console.log(voted);
             console.log(votes);
-            if (voted.find(function (x) { return x == name_1; })) {
+            if ((voted as any).find(x => x == name)) {
                 console.log("Found this playa");
                 return;
             }
-            var re = /!vote (\d+)/;
-            var match = message.match(re);
-            if (!match)
-                return;
-            var realDealMatch = message.match(re)[1];
-            var choice_1 = parseInt(realDealMatch, 10);
+            const re = /!vote (\d+)/;
+            const match = message.match(re);
+            if (!match) return;
+            const realDealMatch = message.match(re)[1];
+            const choice = parseInt(realDealMatch, 10);
             // Is this a valid choice
-            if (!choices[choice_1 - 1])
-                return;
+            if (!choices[choice - 1]) return;
             // Push the username to voted so they can't vote again
-            voted.push(name_1);
-            votes[choices[choice_1 - 1]] = votes[choices[choice_1 - 1]] + 1;
+            voted.push(name);
+            votes[choices[choice - 1]] = votes[choices[choice - 1]] + 1;
             // Save to localStorage
             storage.setItem('votes', votes);
             storage.setItem('voted', voted);
-            runAndTellThat(name_1 + " voted, " + voteAnnouncement(votes));
+            runAndTellThat(`${name} voted, ${voteAnnouncement(votes)}`);
             console.log(votes);
         }
         if (message.startsWith("!choices")) {
             runAndTellThat("1: big timer bot, 2: Paintball Buy/Sell/Trade, 3: Bitcoin miner, 4: chat game");
         }
     });
-});
+})
